54a0558a01ed078b9acc61386406b342dd7afff8
home-yu <y90650386@gmail.com>
2024-12-10 20:43:25 +0900

    open dev server

--- src/router.go
+++ src/router.go
@@ -1,2 +1,17 @@
 package main
 
+import (
+	"net/http"
+	
+	"github.com/labstack/echo/v4"
+)
+
+func newRouter() *echo.Echo {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	
+	return e
+}
