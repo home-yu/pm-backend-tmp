2f41e1df25b647c1a40039b37d514b4bc0126b97
ngm <ngm9ed1128@gmail.com>
2024-12-16 20:12:26 +0900

    rendering

---
+++ src/render.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func Renderer() *Template {
+	renderer := &Template{
+		templates: template.Must(template.ParseGlob("tmp/*.html")),
+	}
+
+	return renderer
+}
